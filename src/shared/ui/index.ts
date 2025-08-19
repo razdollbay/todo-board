8851bedae51415047a840dedabba50265260f157
Oleksiy Nikolaenko <aleshka.dev@gmail.com>
2025-08-19 10:24:32 +0300

    added dialog , spinner and input components

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,6 @@
 export { Button } from './button';
+export * from './dialog';
 export * from './drawer';
+export { Input } from './input';
 export { NavLink } from './nav-link';
+export { Spinner } from './spinner';
